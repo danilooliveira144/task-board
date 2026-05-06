27571d57779a0646ba830f1962f7ff9d794e89b1
Danilo Oliveira <danilooliveira4586@gmail.com>
2026-05-05 21:25:11 -0300

    Implementando a funcionalidade de listar as tarefas

--- Api/routes/web.php
+++ Api/routes/web.php
@@ -9,7 +9,6 @@ $router = new Router();
 // Criar tarefa
 $router->post('/tasks', 'TaskController@store');
 
-// (já vamos deixar preparado pro próximo passo)
 $router->get('/tasks', 'TaskController@index');
 
 return $router;
